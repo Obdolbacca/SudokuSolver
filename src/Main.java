bea3dfa61eb0da7beba024cb881721b111a53aa9
Oleg Bobok <bobizma@gmail.com>
2012-05-18 15:15:03 +0400

    Runnable

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bob
+ * Date: 18.05.12
+ * Time: 15:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main (String[] args) {
+        SudokuSolverClass.sscMain(args);
+        return;
+    }
+}
